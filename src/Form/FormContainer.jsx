ad2dd498b66f568c47795ff17ff8101f531515f6
shinsouhitoshi1203 <sinnohsatoshi.1203@gmail.com>
2024-12-20 13:12:09 +0700

    initialize shit

--- src/Form/FormContainer.jsx
+++ src/Form/FormContainer.jsx
@@ -3,6 +3,7 @@ function FormContainer({ children }) {
      return (
           <div className="formlog">
                <div className="formlog__selector">
+                    
                     <button className="formlog__select">Sign Up</button>
                     <button className="formlog__select">Login</button>
                </div>
